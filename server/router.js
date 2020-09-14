6b0a27d910b8b90e5554538964928aa21d808ce7
Alex <pich.alex01@gmail.com>
2020-09-14 16:35:18 -0700

    Created route to check if token authorization works

--- server/router.js
+++ server/router.js
@@ -1,5 +1,13 @@
+const passport = require("passport");
+const passportService = require("./services/passport");
 const Authentication = require("./controllers/authentication");
 
+const requireAuth = passport.authenticate("jwt", { session: false });
+
 module.exports = function (app) {
+  app.get("/", requireAuth, function (req, res) {
+    res.send({ it: "works" });
+  });
+
   app.post("/signup", Authentication.signup);
 };
