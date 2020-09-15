9f54f4480aba320e00443d75b5daedfe3e12e9a7
Alex <pich.alex01@gmail.com>
2020-09-15 12:25:26 -0700

    Setup local strategy and bcrypt to compare passwords

--- server/services/passport.js
+++ server/services/passport.js
@@ -3,6 +3,28 @@ const User = require("../models/user");
 const config = require("../config/config");
 const JwtStrategy = require("passport-jwt").Strategy;
 const ExtractJwt = require("passport-jwt").ExtractJwt;
+const LocalStrategy = require("passport-local");
+
+// Create local strategy
+const localOptions = { usernameField: "email" };
+const localLogin = new LocalStrategy({ localOptions }, function (
+  email,
+  password,
+  done
+) {
+  // Verify email and password, call done with user if it is correct email and password
+  // otherwise, call done with false
+  User.findOne({ email: email }, function (err, user) {
+    if (err) {
+      return done(err);
+    }
+    if (!user) {
+      return done(null, false);
+    }
+
+    // compare passwords - is 'password' equal to user.password?
+  });
+});
 
 // Setup options for JWT Strategy
 const jwtOptions = {
