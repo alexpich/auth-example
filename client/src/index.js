d86c36edbb9edf9b42b602f26f3b702c504763df
Alex <pich.alex01@gmail.com>
2020-09-16 15:05:19 -0700

    Wiring up middleware

--- client/src/index.js
+++ client/src/index.js
@@ -2,15 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import reduxThunk from "redux-thunk";
 
 import reducers from "./reducers";
 import App from "./components/App";
 import Welcome from "./components/Welcome";
 import Signup from "./components/auth/Signup";
 
+// reducers, initialState, reduxThunk
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
+
 ReactDOM.render(
-  <Provider store={createStore(reducers, {})}>
+  <Provider store={store}>
     <Router>
       <App>
         <Route path="/" exact component={Welcome} />
